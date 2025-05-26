23a5ba02fb4fd4cf93fc835f88c844f519e42fbe
claire <claire.kimani@student.moringaschool.com>
2025-05-26 13:02:54 +0300

    changes in create sql file, insert sql file and sql_queries.py

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, 'Mr. Chocolate', 5, 'M', 'Brown', 'Calm', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, 'Rowdy', 3, 'M', 'Black', 'Aggressive', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, 'Tabitha', 7, 'F', 'Golden', 'Gentle', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, 'Sergeant Brown', 8, 'M', 'Brown', 'Stern', 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, 'Melissa', 4, 'F', 'Black', 'Playful', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, 'Grinch', 6, 'M', 'Gray', 'Grumpy', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, 'Wendy', 2, 'F', 'White', 'Friendly', 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, NULL, 9, 'F', 'Brown', 'Shy', 0);
